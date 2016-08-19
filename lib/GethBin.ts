218e2e150366678158dd694c92d7a50861acde5a
Marius Darila <marius.darila@gmail.com>
2016-08-19 13:33:13 +0300

    feat(binaries): add new binary download source and executable name handling

--- lib/GethBin.ts
+++ lib/GethBin.ts
@@ -6,35 +6,86 @@ import url = require('url');
 
 const defaultTarget = path.join(__dirname, 'bin');
 
-const repo = 'https://github.com/ethereum/go-ethereum/releases/download/';
-const gethVersion = 'v1.4.9/';
-
-const baseUrl = url.resolve(repo, gethVersion);
+const repo = 'https://bintray.com/artifact/download/karalabe/ethereum/geth';
+const gethVersion = '1.4.11-stable-fed692f';
+const arch32: string[] = ['x86', 'x32', 'ia32'];
 
 const source = {
-    linux: 'geth-Linux64-20160629125400-1.4.9-b7e3dfc.tar.bz2',
-    win: 'Geth-Win64-20160629124822-1.4.9-b7e3dfc.zip',
-    osx: 'geth-OSX-2016061509421-1.4.7-667a386.zip'
+    darwin386: 'darwin-10.6-386.tar.bz2',
+    darwin64: 'darwin-10.6-amd64.tar.bz2',
+    linux386: 'linux-386.tar.bz2',
+    linux64: 'linux-amd64.tar.bz2',
+    win386: 'windows-4.0-386.exe.zip',
+    win64: 'windows-4.0-amd64.exe.zip'
 };
 
 const getDownloadUrl = (archive: string): string => {
-    return url.resolve(baseUrl, archive);
+    return [repo, gethVersion, archive].join('-');
 };
 
 export class GethBin {
     public wrapper: Wrapper;
+    public exec: string;
 
     /**
      * @param target    Folder path for `target` geth executable
      */
     constructor(target: string = defaultTarget) {
+        this._getBinName();
+        if (!this.exec) {
+            throw new Error(`${process.platform} ${process.arch} not supported`);
+        }
+        this.exec = ['geth', gethVersion, this.exec].join('-');
         this.wrapper = new Wrapper()
-            .src(getDownloadUrl(source.linux), 'linux', 'x64')
-            .src(getDownloadUrl(source.win), 'win32', 'x64')
-            .src(getDownloadUrl(source.osx), 'darwin', 'x64')
+            .src(getDownloadUrl(source.linux64), 'linux', 'x64')
+            .src(getDownloadUrl(source.linux386), 'linux', 'x86')
+            .src(getDownloadUrl(source.linux386), 'linux', 'ia32')
+            .src(getDownloadUrl(source.linux386), 'linux', 'x32')
+            .src(getDownloadUrl(source.darwin64), 'darwin', 'x64')
+            .src(getDownloadUrl(source.darwin386), 'darwin', 'x86')
+            .src(getDownloadUrl(source.win64), 'win32', 'x64')
+            .src(getDownloadUrl(source.win386), 'win32', 'x86')
+            .src(getDownloadUrl(source.win386), 'win32', 'ia32')
+            .src(getDownloadUrl(source.win386), 'win32', 'x32')
             .dest(target)
-            .use(process.platform === 'win32' ? 'geth.exe' : 'geth');
+            .use(process.platform === 'win32' ? `${this.exec}.exe` : this.exec);
     }
+
+    /**
+     * get binary name
+     * @private
+     */
+    private _getBinName() {
+        switch (process.platform) {
+            case 'linux':
+                if (arch32.indexOf(process.arch) !== -1) {
+                    this.exec = source.linux386.split('.')[0];
+                }
+                if (process.arch === 'x64') {
+                    this.exec = source.linux64.split('.')[0];
+                }
+                break;
+            case 'darwin':
+                if (arch32.indexOf(process.arch) !== -1) {
+                    this.exec =  source.darwin386.split('.')[0];
+                }
+
+                if (process.arch === 'x64') {
+                    this.exec =  source.darwin64.split('.')[0];
+                }
+                break;
+            case 'win32':
+                if (arch32.indexOf(process.arch) !== -1) {
+                    this.exec = source.win386.split('.')[0];
+                }
+                if (process.arch === 'x64') {
+                    this.exec = source.win64.split('.')[0];
+                }
+                break;
+            default:
+                this.exec = '';
+        }
+    };
 
     /**
      * Required geth version for this app
