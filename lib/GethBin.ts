00aba80eb4092ec96c8b107cbf6e9187f38d416d
Marius Darila <marius.darila@gmail.com>
2017-01-16 13:24:03 +0200

    chore(geth): update version to 1.5.7

--- lib/GethBin.ts
+++ lib/GethBin.ts
@@ -8,14 +8,14 @@ import { unlink } from 'fs';
 const defaultTarget = path.join(__dirname, 'bin');
 
 const repo = 'https://gethstore.blob.core.windows.net/builds/';
-const gethVersion = 'v1.5.6/';
+const gethVersion = 'v1.5.7/';
 const unlinkAsync = Promise.promisify(unlink);
 // const baseUrl = url.resolve(repo, gethVersion);
 
 const source = {
-    linux: repo + 'geth-linux-amd64-1.5.6-2a609af5.tar.gz',
-    win: repo + 'geth-windows-amd64-1.5.6-2a609af5.zip',
-    osx: repo + 'geth-darwin-amd64-1.5.6-2a609af5.tar.gz'
+    linux: repo + 'geth-linux-amd64-1.5.7-da2a22c3.tar.gz',
+    win: repo + 'geth-windows-amd64-1.5.7-da2a22c3.zip',
+    osx: repo + 'geth-darwin-amd64-1.5.7-da2a22c3.tar.gz'
 };
 
 export class GethBin {
