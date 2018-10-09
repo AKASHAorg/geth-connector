210523b0bbf118782c42ff0b9379785a5695206c
Marius Darila <marius.darila@gmail.com>
2018-10-09 15:19:05 +0300

    chore(): upgrade geth to v1.8.17

--- lib/GethBin.ts
+++ lib/GethBin.ts
@@ -8,8 +8,8 @@ import { unlink } from 'fs';
 const defaultTarget = path.join(__dirname, 'bin');
 
 const repo = 'https://gethstore.blob.core.windows.net/builds/';
-const gethVersion = '1.8.16';
-const gethCommit = '477eb093';
+const gethVersion = '1.8.17';
+const gethCommit = '8bbe7207';
 // const baseUrl = url.resolve(repo, gethVersion);
 
 const source = {
